ec62612c96eea279b6a3fee6a56d83fff87524a4
Patricia milou <patriciamilou97@gmail.com>
2020-03-03 11:03:03 +0000

    receiver2a done?

--- part2/Receiver2a.java
+++ part2/Receiver2a.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.net.*;
 
-public class Receiver1b {
+public class Receiver2a {
     public static void main(String args[]) throws Exception {
 
         // Get the address, port and name of file to send over UDP
@@ -10,82 +10,87 @@ public class Receiver1b {
 
         receiveFile(port, fileName);
     }
-    public static void sendAckPacket(int previousSequenceNumber, DatagramSocket receiverSocket, InetAddress hostAddress, int portNumber) throws IOException {
+
+    public static void sendAckPacket(int previousSequenceNumber, DatagramSocket receiverSocket, InetAddress hostAddress,
+            int portNumber) throws IOException {
         // Resend acknowledgement
         byte[] ackPacketToSend = new byte[2];
-        ackPacketToSend[0] = (byte)(previousSequenceNumber >> 8);
-        ackPacketToSend[1] = (byte)(previousSequenceNumber);
-        DatagramPacket acknowledgement = new  DatagramPacket(ackPacketToSend, ackPacketToSend.length, hostAddress, portNumber);
+        ackPacketToSend[0] = (byte) (previousSequenceNumber >> 8);
+        ackPacketToSend[1] = (byte) (previousSequenceNumber);
+        DatagramPacket acknowledgement = new DatagramPacket(ackPacketToSend, ackPacketToSend.length, hostAddress,
+                portNumber);
         receiverSocket.send(acknowledgement);
     }
 
     public static void receiveFile(int port, String fileName) throws Exception {
-    //   create receiver socket
-      DatagramSocket receiverSocket = new DatagramSocket(port);
-      File file = new File(fileName);
-      FileOutputStream fileStream = new FileOutputStream(file);
+        // create receiver socket
+        DatagramSocket receiverSocket = new DatagramSocket(port);
+        File file = new File(fileName);
+        FileOutputStream fileStream = new FileOutputStream(file);
 
-      //sequence numbers and flag
-      int sequenceNumber = 0;
-      int previousSequenceNumber = 0;
-      boolean flagLastMessage = false;
-      boolean lastMessage = false;
-      // for each incoming message
-      while (!lastMessage) {
-          byte[] buffer = new byte[1027];
-          // Receive packet and retrieve message
-          DatagramPacket receivedPacket = new DatagramPacket(buffer, buffer.length);
-          receiverSocket.setSoTimeout(0);
-          receiverSocket.receive(receivedPacket);
+        // sequence numbers and flag
+        int sequenceNumber = -1;
+        // int previousSequenceNumber = 0;
+        int expectedSequenceNum = 1;
+        boolean flagLastMessage = false;
+        boolean lastMessage = false;
+        // for each incoming message
+        while (!lastMessage) {
+            byte[] buffer = new byte[1027];
+            // Receive packet and retrieve message
+            DatagramPacket receivedPacket = new DatagramPacket(buffer, buffer.length);
+            receiverSocket.setSoTimeout(0);
+            receiverSocket.receive(receivedPacket);
 
-          // retrieve portNumber and hostAddress for sending ack back
-          int portNumber = receivedPacket.getPort();
-          InetAddress hostAddress = receivedPacket.getAddress();
+            // retrieve portNumber and hostAddress for sending ack back
+            int portNumber = receivedPacket.getPort();
+            InetAddress hostAddress = receivedPacket.getAddress();
 
-          byte[] messageReceived = new byte[receivedPacket.getLength()];
-          byte[] dataReceived = new byte[receivedPacket.getLength() - 3];
+            byte[] messageReceived = new byte[receivedPacket.getLength()];
+            byte[] dataReceived = new byte[receivedPacket.getLength() - 3];
 
-          messageReceived = receivedPacket.getData();
-          int sequenceNumberA = (messageReceived[0] & 0xff) << 8;
-          int sequenceNumberB = (messageReceived[1] & 0xff);
-          sequenceNumber = sequenceNumberA + sequenceNumberB;
+            messageReceived = receivedPacket.getData();
+            int sequenceNumberA = (messageReceived[0] & 0xff) << 8;
+            int sequenceNumberB = (messageReceived[1] & 0xff);
+            sequenceNumber = sequenceNumberA + sequenceNumberB;
 
-          if ((previousSequenceNumber+1) ==  sequenceNumber) {
-              previousSequenceNumber = sequenceNumber;
-              // check header to see if it's the last message
-              if ((messageReceived[2] & 0xff) == 1) {
-                  flagLastMessage = true;
-              } else {
-                  flagLastMessage = false;
-              }
-              // get data from message received
+            if ((expectedSequenceNum) == sequenceNumber) {
+                // check header to see if it's the last message
+                if ((messageReceived[2] & 0xff) == 1) {
+                    flagLastMessage = true;
+                } else {
+                    flagLastMessage = false;
+                }
+                // get data from message received
 
-              for (int i=0; i < dataReceived.length; i++) {
-                  dataReceived[i] = messageReceived[i+3];
-              }
+                for (int i = 0; i < dataReceived.length; i++) {
+                    dataReceived[i] = messageReceived[i + 3];
+                }
 
-              // save data into a new file with name as fileName
-              fileStream.write(dataReceived);
-              // Send acknowledgement
-              sendAckPacket(previousSequenceNumber, receiverSocket, hostAddress, portNumber);
+                // save data into a new file with name as fileName
+                fileStream.write(dataReceived);
+                // Send acknowledgement
+                sendAckPacket(expectedSequenceNum, receiverSocket, hostAddress, portNumber);
+                expectedSequenceNum += 1;
 
             } else {
-                  //Resend the acknowledgement
-                  sendAckPacket(previousSequenceNumber, receiverSocket, hostAddress, portNumber);
-                  flagLastMessage = false;
+                // Resend the acknowledgement
+                sendAckPacket(expectedSequenceNum, receiverSocket, hostAddress, portNumber);
+                flagLastMessage = false;
             }
-              // if it was the last message to be received close file stream
-              if (flagLastMessage) {
-              fileStream.close();
-              receiverSocket.close();
-              lastMessage = true;
-          }
-      }
+            // if it was the last message to be received close file stream
+            if (flagLastMessage) {
+                fileStream.close();
+                receiverSocket.close();
+                lastMessage = true;
+            }
+        }
 
-      // close socket once done
-      receiverSocket.close();
-      // confirmation message
-      // System.out.println("\n=============================== C O M P L E T E D ===============================");
-    //   System.out.println("\n Received: "+fileName);
+        // close socket once done
+        receiverSocket.close();
+        // confirmation message
+        // System.out.println("\n=============================== C O M P L E T E D
+        // ===============================");
+        // System.out.println("\n Received: "+fileName);
     }
 }
